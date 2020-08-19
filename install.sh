2a2ac9a9d26c8c26393ecf777500267d0ab57971
liyunteng <li_yunteng@163.com>
2020-08-20 02:05:22 +0800

    update install.sh, add git clone options

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ if [[ $INSTALL_GIT_REPOS == 0 ]]; then
 fi
 
 github="https://github.com/liyunteng"
+gitopt="--depth=1"
 my_repos=("c" "cpp" "python" "libs" "ffmpeg" "forks")
 if [[ ! -d ~/git ]]; then
     mkdir -p ~/git
@@ -33,18 +34,18 @@ if which git > /dev/null; then
     for x in ${my_repos[@]}; do
         p=~/git/$x
         if [ ! -d $p ]; then
-            git clone $github/$x $p
+            git clone $gitopt $github/$x $p
         fi
     done
 
     if [[ ! -d ~/.vim_runtime ]]; then
-        git clone https://github.com/liyunteng/vim ~/.vim_runtime
+        git clone $gitopt https://github.com/liyunteng/vim ~/.vim_runtime
         ~/.vim_runtime/install_awesome_parameterized.sh ~/.vim_runtime "$USER"
         cd ~/.vim_runtime && python3 update_plugins.py && cd -
     fi
 
     if [[ ! -d ~/.oh-my-zsh ]]; then
-        git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+        git clone $gitopt https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
         ~/.oh-my-zsh/tools/install.sh > /dev/null
     fi
 
@@ -54,7 +55,7 @@ if which git > /dev/null; then
     fi
 
     if [[ ! -d ~/.emacs.d ]]; then
-        git clone https://github.com/liyunteng/emacs ~/.emacs.d
+        git clone $gitopt https://github.com/liyunteng/emacs ~/.emacs.d
         cd ~/.emacs.d && git checkout develop & cd -
         emacs --debug-init && emacsclient -e "(kill-emacs)"
     fi
