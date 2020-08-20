b809849f2d399abc12007da5104569ff6f1fe3e2
liyunteng <li_yunteng@163.com>
2020-08-20 21:51:14 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,70 +4,104 @@
 # Copyright (C) 2019 liyunteng
 # Last-Updated: <2019/11/16 03:19:02 liyunteng>
 set -e
+
+function install_configs() {
+    local my_configs=(".bash_profile" ".bashrc" ".clang-format" ".gitconfig" ".git-credentials" ".tmux.conf"
+    ".curlrc" ".wgetrc" ".editorconfig" "zsh/.zshrc")
+    local target="${HOME}"
+    for x in ${my_configs[@]}; do
+        cp -fr ${x} ${target}
+    done
+}
+
+function check_git_cmd() {
+    if [[ ! git ]]; then
+        echo "Please install 'git' first!"
+        exit -1
+    fi
+}
+
+function install_repos() {
+    local my_repos=("c" "cpp" "python" "libs" "ffmpeg" "forks")
+    local github="https://github.com/liyunteng"
+    local gitopt="--depth=1"
+    local gitdir="${HOME}/git"
+    local target
+
+    check_git_cmd
+    if [[ ! -d ${gitdir} ]]; then
+        mkdir -p ${gitdir}
+    fi
+
+    for x in ${my_repos[@]}; do
+        target=${gitdir}/${x}
+        if [[ ! -d ${target} ]]; then
+            git clone ${gitopt} ${github}/${x} ${target}
+        fi
+    done
+}
+
+function install_vim() {
+    local url="https://github.com/liyunteng/vim"
+    local gitopt="--depth=1"
+    local target="${HOME}/.vim_runtime"
+
+    check_git_cmd
+    if [[ ! -d ${target} ]]; then
+        git clone ${gitopt} ${url} ${target}
+        ${target}/install_awesome_parameterized.sh ${target} "$USER"
+        python3 ${target}/update_plugins.py
+    fi
+}
+
+function install_zsh() {
+    local url="https://github.com/robbyrussell/oh-my-zsh"
+    local gitopt="--depth=1"
+    local target="${HOME}/.oh-my-zsh"
+
+    check_git_cmd
+    if [[ ! -d ${target} ]]; then
+        git clone ${gitopt} ${url} ${target}
+        ${target}/tools/install.sh > /dev/null
+    fi
+
+    if [[ -d ${target} ]]; then
+        cp -fr zsh/plugins ${target}
+        cp -fr zsh/themes ${target}
+    fi
+}
+
+function install_emacs() {
+    local url="https://github.com/liyunteng/emacs"
+    local gitopt="--depth=1"
+    local target="${HOME}/.emacs.d"
+
+    check_git_cmd
+    if [[ ! -d ${target} ]]; then
+        git clone $gitopt ${url} ${target}
+        cd ${target} && git checkout develop & cd -
+        emacs --debug-init && emacsclient -e "(kill-emacs)"
+    fi
+}
+
+
+###############################
 INSTALL_GIT_REPOS=1
 
 if [[ $# -ge 1 && $1 == "-n" ]]; then
     INSTALL_GIT_REPOS=0
 fi
 
-cp -fr .bash_profile ~/
-cp -fr .bashrc ~/
-cp -fr .clang-format ~/
-cp -fr .gitconfig ~/
-cp -fr .git-credentials ~/
-cp -fr .gitignore ~/
-cp -fr .tmux.conf ~/
-cp -fr .curlrc ~/
-cp -fr .editorconfig ~/
-cp -fr .wgetrc ~/
-cp -fr zsh/.zshrc ~/
 
+install_configs
 if [[ $INSTALL_GIT_REPOS == 0 ]]; then
     echo "install success"
     exit 0
 fi
+install_repos
+install_zsh
+install_vim
+install_emacs
 
-github="https://github.com/liyunteng"
-gitopt="--depth=1"
-my_repos=("c" "cpp" "python" "libs" "ffmpeg" "forks")
-if [[ ! -d ~/git ]]; then
-    mkdir -p ~/git
-fi
 
-if which git > /dev/null; then
-    for x in ${my_repos[@]}; do
-        p=~/git/$x
-        if [ ! -d $p ]; then
-            git clone $gitopt $github/$x $p
-        fi
-    done
-
-    if [[ ! -d ~/.vim_runtime ]]; then
-        git clone $gitopt https://github.com/liyunteng/vim ~/.vim_runtime
-        ~/.vim_runtime/install_awesome_parameterized.sh ~/.vim_runtime "$USER"
-        cd ~/.vim_runtime && python3 update_plugins.py && cd -
-    fi
-
-    if [[ ! -d ~/.oh-my-zsh ]]; then
-        git clone $gitopt https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
-        ~/.oh-my-zsh/tools/install.sh > /dev/null
-    fi
-
-    if [[ -d ~/.oh-my-zsh ]]; then
-        cp -fr zsh/plugins ~/.oh-my-zsh/
-        cp -fr zsh/themes ~/.oh-my-zsh/
-    fi
-
-    if [[ ! -d ~/.emacs.d ]]; then
-        git clone $gitopt https://github.com/liyunteng/emacs ~/.emacs.d
-        cd ~/.emacs.d && git checkout develop & cd -
-        emacs --debug-init && emacsclient -e "(kill-emacs)"
-    fi
-
-    echo "install success"
-    exit 0
-else
-    echo "please install 'git'!"
-    exit -1
-fi
 
