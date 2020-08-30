94fb3a92c71aa703d3335d28c718d58795f45e89
liyunteng <li_yunteng@163.com>
2020-08-30 16:13:22 +0800

    fix can't work correct for ssh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 # Last-Updated: <2019/11/16 03:19:02 liyunteng>
 set -e
 INSTALL_CONFIG=${INSTALL_CONFIG:-yes}
-INSTALL_ZSH=${INSTALL_ZSH:-no}
+INSTALL_ZSH=${INSTALL_ZSH:-yes}
 INSTALL_VIM=${INSTALL_VIM:-no}
 INSTALL_EMACS=${INSTALL_EMACS:-no}
 INSTALL_ALL=${INSTALL_ALL:-no}
@@ -20,18 +20,46 @@ update () {
     git submodule update
 }
 
+# create_link src delete?
 create_link () {
     local src="$(pwd)/$1"
     local target="${HOME}/.$1"
     if [[ $2 && -e ${target} ]]; then
         rm -rf ${target}
-        echo "rm ${target}"
+        echo "rm   ${target}"
     fi
     ln -sf ${src} ${target}
-    echo "install ${target} --> ${src}"
+    echo "link ${src} --> ${target}"
 }
 
-install_configs() {
+
+# create_copy src target
+create_copy () {
+    local src="$(pwd)/$1"
+    local target=$2
+
+    cp -rf ${src} ${target}
+    echo "copy ${src} ==> ${target}"
+}
+
+install_ssh () {
+    local target=${HOME}/.ssh
+
+    if [[ -L ${target} ]]; then
+        rm -rf ${target}
+        echo "rm   ${target}"
+    fi
+    if [[ ! -e ${target} ]]; then
+        mkdir -p ${target}
+    fi
+
+    create_copy ssh/config ${target}
+    create_copy ssh/id_rsa ${target}
+    create_copy ssh/id_rsa.pub ${target}
+    create_copy ssh/aws_test_env.pem ${target}
+}
+
+install_configs () {
     local my_configs=(
         bash_profile bashrc alias.sh zshrc
         gitconfig gitignore
@@ -42,22 +70,16 @@ install_configs() {
         create_link ${x}
     done
 
-
-    # ssh
-    local target=${HOME}/.ssh
-    if [[ -L ${target} ]]; then
-        rm -rf ${target}
-    fi
-    cp -rf ssh ${target}
+    install_ssh
 }
 
 install_zsh() {
     local src=oh-my-zsh
-    local target=${HOME}/.{src}
+    local target=${HOME}/.${src}
 
     create_link ${src} 1
-    cp -af zsh/plugins ${target}
-    cp -af zsh/themes ${target}
+    create_copy zsh/plugins ${target}
+    create_copy zsh/themes ${target}
 }
 
 install_emacs() {
